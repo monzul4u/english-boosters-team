69d14c7347600956b0f0e0e588c1e5af037c934b
monzul4u <monzul4u@gmail.com>
2021-03-10 21:29:35 +0000

    added

--- src/components/TeamDetail/TeamDetail.js
+++ src/components/TeamDetail/TeamDetail.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee} from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react';
 import { useParams } from 'react-router';
+
 
 
 const TeamDetail = () => {
@@ -16,9 +19,16 @@ const TeamDetail = () => {
 
     return (
         <div>
-          
-          <h2 className="text-center">This is Team Id details{id}</h2>
-          <h2>{team.strCountry}</h2>
+          <img style={{width:'100%', height:'150px'}} src={team.strTeamBanner} alt=""/>
+          <h2>Founded:{team.intFormedYear} </h2>
+          <h2>Country:{team.strCountry}</h2>
+          <h2>Sports Type:{team.strSport}</h2>
+          <h2>Gender:{team.strGender}</h2>
+           <p>{team.strDescriptionEN}</p>
+            <h2 className="text-center"><FontAwesomeIcon icon={faCoffee} /> <FontAwesomeIcon icon={faCoffee} /> <FontAwesomeIcon icon={faCoffee} /></h2>
+            
+            
+
           
           
              
